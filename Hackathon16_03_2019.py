0bcdfa5a5bf1b6545fe89e13e20674365b933cac
EJRMdlRR <ivanbenji2@gmail.com>
2019-03-16 11:10:51 -0400

    Weight function

--- Hackathon16_03_2019.py
+++ Hackathon16_03_2019.py
@@ -1,4 +1,8 @@
+grade_alphabet = ['A','B','C','D','F']
+
 def weight(class_results):
+    num_results = []
     for result in class_results:
-        alphabet.index[result]
-testing, will this show up in github
+        grade_score = grade_alphabet.index[result] - grade_alphabet.index[goal]
+        num_results.append(grade_score)
+    weight = sum(num_results) / len(num_results)
