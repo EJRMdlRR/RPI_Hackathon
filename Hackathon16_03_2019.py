30518ab441ea2a4b7b5a5243f2b3a323e5e31e2d
EJRMdlRR <ivanbenji2@gmail.com>
2019-03-16 14:38:36 -0400

    Weighting complete

--- Hackathon16_03_2019.py
+++ Hackathon16_03_2019.py
@@ -1,8 +1,27 @@
+from datetime import datetime
+import math
+
 grade_alphabet = ['A','B','C','D','F']
 
-def weight(class_results):
+def time_weight(start, day, end):
+    beginning = datetime.strptime(start, "%Y%m%d")
+    current = datetime.strptime(day, "%Y%m%d")
+    finale = datetime.strptime(end, "%Y%m%d")
+    diff = abs((current - beginning).days)
+    length = abs((finale - beginning).days)
+    time_weight = math.sqrt(diff / length)
+    return time_weight
+
+def total_weight(goal, class_results):
     num_results = []
     for result in class_results:
-        grade_score = grade_alphabet.index[result] - grade_alphabet.index[goal]
+        grade_score = time_weight(day1, result[0], end) * (grade_alphabet.index(result[1]) - grade_alphabet.index(goal))
         num_results.append(grade_score)
     weight = sum(num_results) / len(num_results)
+    if weight <= 0:
+        weight += -1
+    return weight
+
+goal = input('Please enter desired grade: ')
+c_r = ['A','B','B','A']
+print(weight(goal, c_r))
