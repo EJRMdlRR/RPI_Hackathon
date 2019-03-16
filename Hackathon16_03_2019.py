89d4d69e33f3d7e99dc12c7befb8feb8746abfd0
Tinashe Osewe <t.osewe1@gmail.com>
2019-03-16 17:37:04 -0400

    finished Django file setup

--- Hackathon16_03_2019.py
+++
@@ -1,4 +0,0 @@
-def weight(class_results):
-    for result in class_results:
-        result_input = alphabet.index[result]
-    alphabet = result_input * .75
---
+++ halfchance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HalfchanceConfig(AppConfig):
+    name = 'halfchance'
